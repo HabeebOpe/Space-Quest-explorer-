68d21802c08ca7a68a87043a9fb9cf71fb96e488
habeebullahokunlola@gmail.com <habeebullahokunlola@gmail.com>
2023-10-26 10:06:24 +0100

    edited the nav-bar display effect

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const body = document.querySelector('body');
+const nav = document.querySelector('nav');
 const navBtn = document.querySelector('.open-nav')
+
+let lastScrollPosition = 0;
+function scrollPosition() {
+   const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+   
+   if (currentScrollPosition > lastScrollPosition) {
+      nav.classList.add("disappear")
+   } else {
+      nav.classList.remove("disappear")
+   }
+   lastScrollPosition = currentScrollPosition;
+}
 
 navBtn.addEventListener('click', () => {
    body.classList.toggle('open')
 })
 
 window.addEventListener('scroll', function() {
-    if (window.scrollY >= 200) {
-       body.classList.add("nav-design")
-        // Your scroll position is greater than or equal to 200
-        // You can now apply the function to your nav-bar
-        // For example, you can add a CSS class or modify its style
-    } else {
-       body.classList.remove("nav-design")
-        // Your scroll position is less than 200
-        // You can reset the nav-bar to its initial state
-    }
+   scrollPosition()
+   body.classList.remove('open')
+   if (window.scrollY >= 200) {
+      body.classList.add("nav-design")
+   }
+   else if(window.screenY < 200) {
+      body.classList.remove("nav-design")
+   }
 });
